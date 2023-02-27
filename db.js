396f7caeb576b0888b77a9bf232a4466094a32a4
Abhay <abhaypawar596@gmail.com>
2023-02-27 10:44:01 +0530

    ..

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2"
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "333@abhayabhay",
-  database: "blog"
+  host: process.env.host || "localhost",
+  user: process.env.user || "root",
+  password: process.env.password || "333@abhayabhay",
+  database: process.env.database || "blog"
 })
 // const q = "SELECT * FROM users WHERE email = 'aaa' OR username = 'aaa'";
 // db.query(q, (err, data) => {
