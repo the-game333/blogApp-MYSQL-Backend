c0d2d856fa245cd497c0cd8c806ad6412900edcd
Abhay <abhaypawar596@gmail.com>
2023-02-28 01:36:41 +0530

    ..

--- controllers/post.js
+++ controllers/post.js
@@ -25,11 +25,11 @@ export const getPost = (req, res) => {
 };
 
 export const addPost = (req, res) => {
-  const token = req.cookies.access_token;
- if (!token) return res.status(401).json("Not authenticated!");
+  //const token = req.cookies.access_token;
+ //if (!token) return res.status(401).json("Not authenticated!");
 
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-   if (err) return res.status(403).json("Token is not valid!");
+  //jwt.verify(token, "jwtkey", (err, userInfo) => {
+   //if (err) return res.status(403).json("Token is not valid!");
 
     const q =
       "INSERT INTO posts(`title`, `descr`, `img`, `cat`, `date`,`uid`) VALUES (?)";
@@ -47,7 +47,7 @@ export const addPost = (req, res) => {
       if (err) return res.status(500).json(err);
       return res.json("Post has been created.");
     });
-  });
+ // });
 };
 
 export const deletePost = (req, res) => {
